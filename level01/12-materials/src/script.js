94548060c2cf7c4884e15d94c53664ef57464e8e
Hulusi Kafalıer <hulusikafalier@gmail.com>
2021-07-05 20:42:20 +0300

    12. Materials(finish)

--- level01/12-materials/src/script.js
+++ level01/12-materials/src/script.js
@@ -1,6 +1,38 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+// Debug
+const gui = new dat.GUI()
+
+// Textures
+const loadingManager = new THREE.LoadingManager()
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorAmbientTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
+
+gradientTexture.minFilter = THREE.NearestFilter
+gradientTexture.magFilter = THREE.NearestFilter
+gradientTexture.generateMipmaps = false
+
+const environmentMapTexture = cubeTextureLoader.load([
+  '/textures/environmentMaps/1/px.jpg',
+  '/textures/environmentMaps/1/nx.jpg',
+  '/textures/environmentMaps/1/py.jpg',
+  '/textures/environmentMaps/1/ny.jpg',
+  '/textures/environmentMaps/1/pz.jpg',
+  '/textures/environmentMaps/1/nz.jpg'
+])
 
 /**
  * Base
@@ -11,27 +43,115 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Objects
+// -------------- Mesh Basic Material -----------
+// const material = new THREE.MeshBasicMaterial()
+// material.color = new THREE.Color('red')
+// material.color.set('#f00')
+// material.map = doorColorTexture
+// material.wireframe = true
+// material.transparent = true // Needed to work for opacity, alpha
+// material.opacity = 0.5
+// material.alphaMap = doorAlphaTexture
+// material.side = THREE.DoubleSide
+
+// const material = new THREE.MeshNormalMaterial()
+// material.flatShading = true
+
+// const material = new THREE.MeshMatcapMaterial()
+// material.matcap = matcapTexture
+
+// Close to camera white otherwise black
+// Light does not work
+// const material = new THREE.MeshDepthMaterial()
+
+// const material = new THREE.MeshLambertMaterial() // No light reflection
+
+// const material = new THREE.MeshPhongMaterial() // Less performant
+// material.shininess = 100
+// material.specular = new THREE.Color('#f00')
+
+// const material = new THREE.MeshToonMaterial()
+// material.gradientMap = gradientTexture
+
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.7
+material.roughness = 0.2
+material.envMap = environmentMapTexture
+// material.map = doorColorTexture
+// material.aoMap = doorAmbientTexture
+// material.aoMapIntensity = 1
+// material.displacementMap = doorHeightTexture // height === displacement
+// material.displacementScale = 0.05
+// material.metalnessMap = doorMetalnessTexture
+// material.roughnessMap = doorRoughnessTexture
+// material.normalMap = doorNormalTexture
+// material.normalScale.set(0.5, 0.5)
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+
+gui.add(material, 'metalness', 0, 1, 0.0001)
+gui.add(material, 'roughness', 0, 1, 0.0001)
+gui.add(material, 'aoMapIntensity', 0, 10, 0.0001)
+gui.add(material, 'displacementScale', 0, 10, 0.0001)
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereBufferGeometry(0.5, 62, 62),
+  material
+)
+sphere.position.x = -1.5
+// Ambient Occlusion
+sphere.geometry.setAttribute('uv2',
+  new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry(1, 1, 100, 100),
+  material
+)
+// Ambient Occlusion
+plane.geometry.setAttribute('uv2',
+  new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2))
+
+const torus = new THREE.Mesh(
+  new THREE.TorusBufferGeometry(0.3, 0.2, 64, 128),
+  material
+)
+torus.position.x = 1.5
+// Ambient Occlusion
+torus.geometry.setAttribute('uv2',
+  new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2))
+
+scene.add(sphere, plane, torus)
+
+const ambientLight = new THREE.AmbientLight('#fff', 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight('#fff', 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -41,7 +161,7 @@ window.addEventListener('resize', () =>
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
 camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 3.5
 scene.add(camera)
 
 // Controls
@@ -52,7 +172,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -62,18 +182,26 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+  // Update objects
+  sphere.rotation.y = elapsedTime * 0.1
+  plane.rotation.y = elapsedTime * 0.1
+  torus.rotation.y = elapsedTime * 0.1
 
-    // Render
-    renderer.render(scene, camera)
+  sphere.rotation.x = elapsedTime * 0.15
+  plane.rotation.x = elapsedTime * 0.15
+  torus.rotation.x = elapsedTime * 0.15
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
